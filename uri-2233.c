f186170f60b2ce002aa06709316535d8a93257e5
thiagoprestes <thiagoprestes@thiagohenriqueprestes@gmail.com>
2016-09-17 06:27:38 -0300

    Alterado uri-2233.c

--- uri-2233.c
+++ uri-2233.c
@@ -4,32 +4,32 @@
 #include <string.h>
 
 int main(){
-     long long red, green, blue, total, total2, total3, a = 1;
+     long long red, green, blue, total, total2, total3;
 
      scanf("%llx %llx %llx" , &red, &green, &blue);
-     while(a--){
-	     if(red < green){ printf("1\n"); break;}
+     
+	     if(red < green){ printf("1\n"); return 0;}
 
 	   
 	     total = (long long)pow((long long)((green/blue)),2);
 
 	     if(red == green){
-	     	 if(green == blue){printf("3\n"); break;}
+	     	 if(green == blue){printf("3\n"); return 0;}
 
-	         if(green < blue){printf("2\n"); break;}
+	         if(green < blue){printf("2\n"); return 0;}
 
-	         printf("%llx\n", total + 2); break; 
+	         printf("%llx\n", total + 2); return 0; 
 	     }
 
 	     total2 = (long long)pow((long long)((red/green)),2);
 
-	     if(green < blue){printf("%llx\n", total2+1); break;}
+	     if(green < blue){printf("%llx\n", total2+1); return 0;}
 
-         if(green == blue){printf("%llx\n", total2*2+1); break;}
+         if(green == blue){printf("%llx\n", total2*2+1); return 0;}
 
          total3 = total * total2 + total2 + 1; printf("%llx\n", total3);
 
-     }
+     
 
 	 return 0;
 }
