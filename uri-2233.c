a48ef221326ab373797f3520a45416c26406fc12
Thiago Prestes <thiagohenriqueprestes@gmail.com>
2016-09-17 06:30:37 -0300

    Alterado uri-2233.c

--- uri-2233.c
+++ uri-2233.c
@@ -8,28 +8,26 @@ int main(){
 
      scanf("%llx %llx %llx" , &red, &green, &blue);
      
-	     if(red < green){ printf("1\n"); return 0;}
+     if(red < green){ printf("1\n"); return 0;}
 
-	   
-	     total = (long long)pow((long long)((green/blue)),2);
-
-	     if(red == green){
-	     	 if(green == blue){printf("3\n"); return 0;}
-
-	         if(green < blue){printf("2\n"); return 0;}
-
-	         printf("%llx\n", total + 2); return 0; 
-	     }
-
-	     total2 = (long long)pow((long long)((red/green)),2);
-
-	     if(green < blue){printf("%llx\n", total2+1); return 0;}
-
-         if(green == blue){printf("%llx\n", total2*2+1); return 0;}
-
-         total3 = total * total2 + total2 + 1; printf("%llx\n", total3);
-
+     total = (long long)pow((long long)((green/blue)),2);
      
+     if(red == green){
+	 if(green == blue){printf("3\n"); return 0;}
 
-	 return 0;
+         if(green < blue){printf("2\n"); return 0;}
+
+	 printf("%llx\n", total + 2); return 0; 
+     	
+     }
+ 
+      total2 = (long long)pow((long long)((red/green)),2);
+
+      if(green < blue){printf("%llx\n", total2+1); return 0;}
+
+      if(green == blue){printf("%llx\n", total2*2+1); return 0;}
+
+      total3 = total * total2 + total2 + 1; printf("%llx\n", total3);
+
+      return 0;
 }
