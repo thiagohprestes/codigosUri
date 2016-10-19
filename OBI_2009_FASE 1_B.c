18047f408c2abb10b877236d858bbc23190eb3ed
Thiago Prestes <thiagohenriqueprestes@gmail.com>
2016-10-19 10:50:14 -0200

    Adicionado OBI_2009_FASE 1_F.c

---
+++ OBI_2009_FASE 1_B.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main(){
+     unsigned int n, i; 
+     int bacterias, dias, resp;
+     long double maior = 0., colonia;
+
+     scanf("%u", &n);
+
+     for(i = 0; i < n; i++){
+         scanf("%d %d", &bacterias, &dias);
+         colonia = dias*log(bacterias);
+         if(colonia > maior){
+     		maior = colonia;
+            resp = i;
+     	 }
+     	  //printf("%Lf\n", maior);
+     }
+
+     printf("%d\n", resp);
+
+	 return 0;
+}
