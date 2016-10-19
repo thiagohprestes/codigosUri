1998d3b24724964d1e46bf24e7aa61999fcccf1a
Thiago Prestes <thiagohenriqueprestes@gmail.com>
2016-10-19 11:00:50 -0200

    Adicionado OBI_2009_FASE 1_E.c

---
+++ OBI_2009_FASE 1_E.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main(){
+	 int matriz[510][510] = {0}, n, x, y, i;
+     short resp = 0;
+
+     scanf("%d", &n);
+
+     for(i = 0; i < n; i++){
+     	 scanf("%d %d", &x, &y);
+     	 if(matriz[x][y] == 0) matriz[x][y] = 1;
+     	 else {resp = 1; break;}  	 
+     }
+     
+      printf("%hd\n", resp);
+
+	 return 0;
+}
