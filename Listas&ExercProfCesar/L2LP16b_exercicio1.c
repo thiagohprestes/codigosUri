c8e38d6510a0bc9054936018fd5fd1289dbdada6
Thiago Prestes <thiagohenriqueprestes@gmail.com>
2016-12-01 13:47:25 -0200

    Atualizado L2LP16b_exercicio1.c

--- Listas&ExercProfCesar/L2LP16b_exercicio1.c
+++ Listas&ExercProfCesar/L2LP16b_exercicio1.c
@@ -3,21 +3,21 @@
 #include <math.h>
 #include <string.h>
 
-#define TAM_VETOR 20
+#define TAM_VETOR 5
 
-struct Aluno{
+typedef struct{
 	char ra[14];
 	char nome[41];
 	float notaFinal;
 	int faltas;
-};
+} RegArqAluno;
 
-void imprimirDados(struct Aluno *);
-int compara( const void *, const void *);
+void imprimirDados(RegArqAluno *);
+int compara(const void *, const void *);
 
 int main(){
 	int i;
-	struct Aluno alunos[TAM_VETOR];
+	RegArqAluno alunos[TAM_VETOR];
 	
 	for(i = 0; i < TAM_VETOR; i++){
 		scanf("%s %[^\n\r] %f %d", &alunos[i].ra, &alunos[i].nome, &alunos[i].notaFinal, &alunos[i].faltas);
@@ -27,7 +27,7 @@ int main(){
 	printf("\n\nDADOS DOS ALUNOS ORDENADO CONFORME ORDEM DE ENTRADA\n\n");
 	imprimirDados(alunos);
 	
-	qsort(alunos, TAM_VETOR, sizeof(struct Aluno), compara);
+	qsort(alunos, TAM_VETOR, sizeof(RegArqAluno), compara);
 	
 	//Imprimindo alunos ordenados descendentemente por nota em ordem alfabética
 	printf("\n\nDADOS DOS ALUNOS ORDENADO CONFORME CRITERIOS\n\n");
@@ -36,7 +36,7 @@ int main(){
 	return 0;
 }
 
-void imprimirDados(struct Aluno *alunos){
+void imprimirDados(RegArqAluno *alunos){
 	int i;
 	for(i = 0; i < TAM_VETOR; i++){
 		printf("RA: %s // NOME: %s // NOTA FINAL: %.1f // FALTAS: %d\n\n", alunos[i].ra, alunos[i].nome, alunos[i].notaFinal, alunos[i].faltas);
@@ -44,7 +44,7 @@ void imprimirDados(struct Aluno *alunos){
 }
 
 int compara( const void *p1, const void *p2 ){
-	struct Aluno *i = (struct Aluno *)p1, *j = (struct Aluno *)p2;
+	RegArqAluno *i = (RegArqAluno *)p1, *j = (RegArqAluno *)p2;
 	if(i->notaFinal < j->notaFinal)
 		return 1;
 	else if(i->notaFinal > j->notaFinal)
